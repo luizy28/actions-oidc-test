236808bf4fe4c7c7555b6d0459460476f2967361
LouOz <louozor@gmail.com>
2023-09-21 13:10:26 -0500

    updated

--- environments/vpc/backend.tf
+++ environments/vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "bootcamp32-dev-13"
-    region  = "us-east-1"
+    region  = "us-west-2"
     key     = "oidc/terraform.tfstate"
     encrypt = true
   }
