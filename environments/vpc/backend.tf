9baae7cef4ede5fb3f8fad431f5d94bef7bf7c0a
LouOz <louozor@gmail.com>
2023-09-21 14:08:31 -0500

    ssh update

--- environments/vpc/backend.tf
+++ environments/vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "bootcamp32-dev-13"
-    region  = "us-west-2"
+    region  = "us-east-1"
     key     = "oidc/terraform.tfstate"
     encrypt = true
   }
